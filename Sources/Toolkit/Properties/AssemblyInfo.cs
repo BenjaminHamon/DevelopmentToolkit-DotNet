0f38e5e7e46fc401510d30e4313f5177c8475633
Benjamin Hamon <development@benjaminhamon.com>
2025-03-13 17:58:58 +0100

    Add dotnet solution base

---
+++ Sources/Toolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BenjaminHamon.DevelopmentToolkit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Benjamin Hamon 2025")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("436f0d1a-ba46-4530-81f6-fa5be3fd6d7c")]
---
+++ Tests/ToolkitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BenjaminHamon.DevelopmentToolkit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Benjamin Hamon 2025")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("10fcb0f9-9930-4a48-84a8-640e419cb610")]
